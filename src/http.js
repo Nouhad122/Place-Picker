bb2936045b10952f7902c818827f7e4ce6e40ba4
Nouhad122 <nouhadalhallab122@gmail.com>
2025-01-04 00:16:57 +0300

    Fetched updateUserPlace method to update user places

--- src/http.js
+++ src/http.js
@@ -7,3 +7,20 @@ export const fetchAvailableData = async () =>{
         }
     return resData.places;
 }
+
+export const updateUserPlace = async (places) =>{
+    const response = await fetch("http://localhost:3000/user-places",{
+        method: "PUT",
+        body: JSON.stringify({places}),
+        headers: {
+            'Content-Type': "application/json"
+        }
+    });
+
+    const resData = await response.json();
+
+    if(!response.ok){
+        throw new Error("Failed to fetch Data");
+    }
+    return resData.message;
+}
